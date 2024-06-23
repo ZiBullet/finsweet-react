7d00e9a884670f00386463bfb406f3b078332f83
ZiBullet <zamolidinziadulaev@gmail.com>
2024-06-23 15:43:18 +0500

    service - development - done

---
+++ src/pages/ServicePage/Development.jsx
@@ -0,0 +1,18 @@
+import development1 from "./assets/images/development1.png";
+
+const Development = () => {
+    return (
+        <section className="development" id="development">
+            <div className="development__wrap container">
+                <img src={development1} alt="development1" className="development__img" />
+                <div className="development__desc">
+                    <h4 className="development__title-mini title-h4">Developoment</h4>
+                    <h3 className="development__title title-h3">Bring your ideas to reality with certified team of developers, working with latest technologies </h3>
+                    <p className="delevelopment__text text-p">Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Development
--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,12 +1,15 @@
 import Services from "./Services"
 import Process from "../HomePage/Process"
 import TechnicalSupport from "./TechnicalSupport"
+import Development from "./Development"
+
 const ServicePage = () => {
   return (
     <main className="main">
       <Services />
       <Process parentClass={"service-page"} />
       <TechnicalSupport />
+      <Development />
     </main>
   )
 }
